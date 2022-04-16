cf78791e005cbec8fd127b19f4ead8018a389f5b
[Kenneth Murithi] <[Kennethmmuriithi@gmail.com]>
2022-04-16 06:31:57 +0300

    added music database and created a datasource class. Closes #2

--- src/com/hiken/Main.java
+++ src/com/hiken/Main.java
@@ -1,8 +1,16 @@
 package com.hiken;
 
+import com.hiken.model.Datasource;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Datasource datasource = new Datasource();
+        if (!datasource.open()){
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        datasource.close();
     }
 }
---
+++ src/com/hiken/model/Datasource.java
@@ -0,0 +1,46 @@
+package com.hiken.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Datasource {
+    public static final String DB_NAME = "music.db";
+    public static final String CONNECTION_STRING = "Jdbc:sqlite:E:\\Tutorial Materials\\SpringBoot\\MusicJDBCApp" + DB_NAME;
+
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUMS_ID ="_id";
+    public static final String TABLE_ALBUMS_NAME = "name";
+    public static final String TABLE_ALBUMS_ARTIST = "artist";
+
+    public static final String TABLE_ARTISTS ="artists";
+    public static final String TABLE_ARTISTS_ID ="_id";
+    public static final String TABLE_ARTISTS_NAME ="name";
+
+    public static final String TABLE_SONGS ="songs";
+    public static final String TABLE_SONGS_TRACK ="track";
+    public static final String TABLE_SONGS_TITLE ="title";
+    public static final String TABLE_SONGS_ALBUM ="album";
+
+    private Connection conn;
+
+    public boolean open(){
+        try {
+            conn = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        }catch (SQLException e){
+            System.out.println("Couldn't connect to database: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public void close(){
+        try {
+            if (conn != null){
+                conn.close();
+            }
+        }catch (SQLException e){
+            System.out.println("Couldn't close the connection: " + e.getMessage());
+        }
+    }
+}
