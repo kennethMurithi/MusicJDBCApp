e19e30244313072bf5cd0cc15832816ad0a51674
[Kenneth Murithi] <[Kennethmmuriithi@gmail.com]>
2022-04-19 09:09:25 +0300

    Querying all artists from musicDB. Closes #3

--- src/com/hiken/Main.java
+++ src/com/hiken/Main.java
@@ -1,6 +1,9 @@
 package com.hiken;
 
+import com.hiken.model.Artist;
 import com.hiken.model.Datasource;
+
+import java.util.List;
 
 public class Main {
 
@@ -9,6 +12,16 @@ public class Main {
         if (!datasource.open()){
             System.out.println("Can't open datasource");
             return;
+        }
+
+        List<Artist> artists = datasource.queryArtists();
+        if (artists == null){
+            System.out.println("No artists!");
+            return;
+        }
+
+        for (Artist artist : artists){
+            System.out.println("ID = " + artist.getId() + ", name = " + artist.getName());
         }
 
         datasource.close();
--- src/com/hiken/model/Datasource.java
+++ src/com/hiken/model/Datasource.java
@@ -1,26 +1,26 @@
 package com.hiken.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
-    public static final String CONNECTION_STRING = "Jdbc:sqlite:E:\\Tutorial Materials\\SpringBoot\\MusicJDBCApp" + DB_NAME;
+    public static final String CONNECTION_STRING = "Jdbc:sqlite:E:\\Tutorial Materials\\SpringBoot\\MusicJDBCApp\\" + DB_NAME;
 
     public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUMS_ID ="_id";
-    public static final String TABLE_ALBUMS_NAME = "name";
-    public static final String TABLE_ALBUMS_ARTIST = "artist";
+    public static final String COLUMN_ALBUMS_NAME = "name";
+    public static final String COLUMN_ALBUMS_ARTIST = "artist";
 
     public static final String TABLE_ARTISTS ="artists";
-    public static final String TABLE_ARTISTS_ID ="_id";
-    public static final String TABLE_ARTISTS_NAME ="name";
+    public static final String COLUMN_ARTISTS_ID ="_id";
+    public static final String COLUMN_ARTISTS_NAME ="name";
 
     public static final String TABLE_SONGS ="songs";
-    public static final String TABLE_SONGS_TRACK ="track";
-    public static final String TABLE_SONGS_TITLE ="title";
-    public static final String TABLE_SONGS_ALBUM ="album";
+    public static final String COLUMN_SONGS_TRACK ="track";
+    public static final String COLUMN_SONGS_TITLE ="title";
+    public static final String COLUMN_SONGS_ALBUM ="album";
 
     private Connection conn;
 
@@ -41,6 +41,24 @@ public class Datasource {
             }
         }catch (SQLException e){
             System.out.println("Couldn't close the connection: " + e.getMessage());
+        }
+    }
+
+    public List<Artist> queryArtists(){
+        try (Statement statement = conn.createStatement();
+             ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_ARTISTS)){
+            List<Artist> artists = new ArrayList<>();
+            while (results.next()){
+                Artist artist = new Artist();
+                artist.setId(results.getInt(COLUMN_ARTISTS_ID));
+                artist.setName(results.getString(COLUMN_ARTISTS_NAME));
+                artists.add(artist);
+            }
+
+            return artists;
+        }catch (SQLException e){
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
         }
     }
 }
